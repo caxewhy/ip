437823e86d1c4db1b072d7571e7a1960d0581339
caxewhy <chanxiaoyong@u.nus.edu>
2025-01-21 23:04:56 +0800

    Did Level 3. Added ability to mark/unmark tasks as done.

--- src/main/java/Solyu.java
+++ src/main/java/Solyu.java
@@ -3,16 +3,12 @@ import java.util.ArrayList;
 
 public class Solyu {
     public static void main(String[] args) {
-        ArrayList<String> tasks = new ArrayList<String>();
+        ArrayList<Task> tasks = new ArrayList<Task>();
 
         String greet = "____________________________________________________________\n"
                 + " Hello! I'm Solyu \n"
                 + " What can I do for you? \n"
                 + "____________________________________________________________\n";
-
-//        String list = "____________________________________________________________\n"
-//                + " list\n"
-//                + "____________________________________________________________\n";
 
         String blah = "____________________________________________________________\n"
                 + " blah\n"
@@ -28,34 +24,83 @@ public class Solyu {
 
         while (true) {
             System.out.print(" Enter a command:");
-            String userInput = scanner.nextLine().trim().toLowerCase();
+            String userInput = scanner.nextLine().trim();
+            String[] parts = userInput.split(" ", 2);
+            String command = parts[0].toLowerCase();
+            String argument = parts.length > 1 ? parts[1] : "";
 
-            switch (userInput) {
+            switch (command) {
                 case "greet":
                     System.out.println(greet);
                     break;
 
                 case "add":
-                    System.out.println("Task to be added: ");
-                    String task = scanner.nextLine();
-                    tasks.add(task);
-                    System.out.println("____________________________________________________________");
-                    System.out.println("Task added to list: " + task);
-                    System.out.println("____________________________________________________________");
+                    if (!argument.isEmpty()) {
+                        tasks.add(new Task(argument));
+                        System.out.println("____________________________________________________________");
+                        System.out.println("Task added: " + argument);
+                        System.out.println("____________________________________________________________");
+                    } else {
+                        System.out.println("____________________________________________________________");
+                        System.out.println("Please specify a task!");
+                        System.out.println("____________________________________________________________");
+                    }
                     break;
 
                 case "list":
+                    System.out.println("____________________________________________________________");
                     if (tasks.isEmpty()) {
-                        System.out.println("____________________________________________________________");
-                        System.out.println("List is empty");
-                    }
-                    else {
-                        System.out.println("____________________________________________________________");
-                        for (int i = 0; i<tasks.size(); i++){
-                            System.out.println((i+1) + ". " + tasks.get(i));
+                        System.out.println("Task list is empty!");
+                    } else {
+                        for (int i = 0; i < tasks.size(); i++) {
+                            System.out.println((i + 1) + ". " + tasks.get(i));
                         }
                     }
                     System.out.println("____________________________________________________________");
+                    break;
+
+                case "mark":
+                    if (!argument.isEmpty()) {
+//                        System.out.println("Task to be marked: ");
+                        int mark = Integer.parseInt(argument) - 1;
+                        if (mark >= 0 && mark < tasks.size()) {
+                            tasks.get(mark).markAsDone();
+                            System.out.println("____________________________________________________________");
+                            System.out.println("Task marked as done: " + tasks.get(mark));
+                            System.out.println("____________________________________________________________");
+                        } else {
+                            System.out.println("____________________________________________________________");
+                            System.out.println("Invalid task!");
+                            System.out.println("____________________________________________________________");
+                        }
+                    }
+                    else {
+                        System.out.println("____________________________________________________________");
+                        System.out.println("Please specify a task!");
+                        System.out.println("____________________________________________________________");
+                    }
+                    break;
+
+                case "unmark":
+                    if (!argument.isEmpty()) {
+//                    System.out.println("Task to be unmarked: ");
+                        int mark = Integer.parseInt(argument) - 1;
+                        if (mark >= 0 && mark < tasks.size()) {
+                            tasks.get(mark).unmark();
+                            System.out.println("____________________________________________________________");
+                            System.out.println("Task unmarked: " + tasks.get(mark));
+                            System.out.println("____________________________________________________________");
+                        } else {
+                            System.out.println("____________________________________________________________");
+                            System.out.println("Invalid task!");
+                            System.out.println("____________________________________________________________");
+                        }
+                    }
+                    else {
+                        System.out.println("____________________________________________________________");
+                        System.out.println("Please specify a task!");
+                        System.out.println("____________________________________________________________");
+                    }
                     break;
 
                 case "blah":
@@ -69,10 +114,11 @@ public class Solyu {
 
                 default:
                     System.out.println("____________________________________________________________");
-                    System.out.println(" Wrong command.");
+                    System.out.println(" No such command: " + command);
                     System.out.println("____________________________________________________________");
                     break;
             }
         }
     }
 }
+
