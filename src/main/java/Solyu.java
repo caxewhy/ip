73bb1f1d4bde235efb3796f30e891e01a2c15ee4
caxewhy <chanxiaoyong@u.nus.edu>
2025-01-21 20:56:45 +0800

    Did Level 1. Added Echo commands.

--- src/main/java/Solyu.java
+++ src/main/java/Solyu.java
@@ -1,11 +1,52 @@
+import java.util.Scanner;
+
 public class Solyu {
     public static void main(String[] args) {
         String greet = "____________________________________________________________\n"
                 + " Hello! I'm Solyu \n"
                 + " What can I do for you? \n"
-                + "____________________________________________________________\n"
-                + " Bye. Hope to see you again soon!";
+                + "____________________________________________________________\n";
+
+        String list = "____________________________________________________________\n"
+                + " list\n"
+                + "____________________________________________________________\n";
+
+        String blah = "____________________________________________________________\n"
+                + " blah\n"
+                + "____________________________________________________________\n";
+
+        String bye = "____________________________________________________________\n"
+                + " Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n";
 
         System.out.println(greet);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print(" Enter a command:");
+            String userInput = scanner.nextLine().trim();
+
+            switch (userInput.toLowerCase()) {
+                case "greet":
+                    System.out.println(greet);
+                    break;
+                case "list":
+                    System.out.println(list);
+                    break;
+                case "blah":
+                    System.out.println(blah);
+                    break;
+                case "bye":
+                    System.out.println(bye);
+                    scanner.close(); // Close the scanner
+                    return; // Exit the program
+                default:
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" Wrong command.");
+                    System.out.println("____________________________________________________________");
+                    break;
+            }
+        }
     }
 }
