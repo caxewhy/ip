796d84dc949f1f452d076d1d6af50018ba900735
caxewhy <chanxiaoyong@u.nus.edu>
2025-01-21 21:25:33 +0800

    Did Level 2. Ability to add tasks, and List them.

--- src/main/java/Solyu.java
+++ src/main/java/Solyu.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Solyu {
     public static void main(String[] args) {
+        ArrayList<String> tasks = new ArrayList<String>();
+
         String greet = "____________________________________________________________\n"
                 + " Hello! I'm Solyu \n"
                 + " What can I do for you? \n"
                 + "____________________________________________________________\n";
 
-        String list = "____________________________________________________________\n"
-                + " list\n"
-                + "____________________________________________________________\n";
+//        String list = "____________________________________________________________\n"
+//                + " list\n"
+//                + "____________________________________________________________\n";
 
         String blah = "____________________________________________________________\n"
                 + " blah\n"
@@ -25,22 +28,45 @@ public class Solyu {
 
         while (true) {
             System.out.print(" Enter a command:");
-            String userInput = scanner.nextLine().trim();
+            String userInput = scanner.nextLine().trim().toLowerCase();
 
-            switch (userInput.toLowerCase()) {
+            switch (userInput) {
                 case "greet":
                     System.out.println(greet);
                     break;
-                case "list":
-                    System.out.println(list);
+
+                case "add":
+                    System.out.println("Task to be added: ");
+                    String task = scanner.nextLine();
+                    tasks.add(task);
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Task added to list: " + task);
+                    System.out.println("____________________________________________________________");
                     break;
+
+                case "list":
+                    if (tasks.isEmpty()) {
+                        System.out.println("____________________________________________________________");
+                        System.out.println("List is empty");
+                    }
+                    else {
+                        System.out.println("____________________________________________________________");
+                        for (int i = 0; i<tasks.size(); i++){
+                            System.out.println((i+1) + ". " + tasks.get(i));
+                        }
+                    }
+                    System.out.println("____________________________________________________________");
+                    break;
+
                 case "blah":
                     System.out.println(blah);
                     break;
+
                 case "bye":
                     System.out.println(bye);
-                    scanner.close(); // Close the scanner
-                    return; // Exit the program
+                    scanner.close();
+                    return;
+
                 default:
                     System.out.println("____________________________________________________________");
                     System.out.println(" Wrong command.");
