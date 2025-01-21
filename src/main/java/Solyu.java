0fd0e5ea344ed48eaf60e0edb484fcd612e42405
caxewhy <chanxiaoyong@u.nus.edu>
2025-01-21 20:32:51 +0800

    Did Level 0. Rename, Greet, Exit.

---
+++ src/main/java/Solyu.java
@@ -0,0 +1,11 @@
+public class Solyu {
+    public static void main(String[] args) {
+        String greet = "____________________________________________________________\n"
+                + " Hello! I'm Solyu \n"
+                + " What can I do for you? \n"
+                + "____________________________________________________________\n"
+                + " Bye. Hope to see you again soon!";
+
+        System.out.println(greet);
+    }
+}
