e2cad57830f77013620b396750999ef5e50a6a2e
caxewhy <chanxiaoyong@u.nus.edu>
2025-01-27 23:03:40 +0800

    Add JUnit tests.

---
+++ src/test/java/solyu/TaskListTest.java
@@ -0,0 +1,32 @@
+package solyu;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TaskListTest {
+
+    @Test
+    public void testAddTask() {
+        TaskList taskList = new TaskList();
+        Task task = new Task("Finish homework");
+        taskList.addTask(task);
+        assertEquals(1, taskList.size(), "Task list should contain Finish homework task.");
+    }
+
+    @Test
+    public void testDeleteTask() {
+        TaskList taskList = new TaskList();
+        Task task = new Task("Go jogging");
+        taskList.addTask(task);
+        taskList.removeTask(0);
+        assertEquals(0, taskList.size(), "Task list should be empty.");
+    }
+
+    @Test
+    public void testGetTask() {
+        TaskList taskList = new TaskList();
+        Task task = new Task("Read a book");
+        taskList.addTask(task);
+        assertEquals(task, taskList.getTask(0), "Retrieved task should be the same as added.");
+    }
+}
