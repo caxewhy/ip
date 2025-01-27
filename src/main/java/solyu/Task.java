d3d75c3886f8fda93c149169bc4bdc6c3ce454de
caxewhy <chanxiaoyong@u.nus.edu>
2025-01-28 00:12:09 +0800

    Add JavaDoc comments to the code.

--- src/main/java/solyu/Task.java
+++ src/main/java/solyu/Task.java
@@ -1,30 +1,63 @@
 package solyu;
 
+/**
+ * This class serves as a base class for specific task types such as ToDo, Event, and Deadline.
+ */
 public class Task {
     protected String description;
     protected boolean isDone;
 
+    /**
+     * Constructs a new Task with the given description.
+     * The task is initially marked as not done.
+     *
+     * @param description The description of the task.
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * Returns the status icon representing whether the task is completed.
+     * "X" indicates completed, while " " indicates not completed.
+     *
+     * @return A string representing the completion status of the task.
+     */
     public String getStatusIcon() {
         return (isDone ? "X" : " ");
     }
 
+    /**
+     * Marks the task as done by setting the status to true.
+     */
     public void markAsDone() {
         this.isDone = true;
     }
 
+    /**
+     * Unmarks the task as done by setting the status to false.
+     */
     public void unmark() {
         this.isDone = false;
     }
 
+    /**
+     * Converts the task details into a formatted string suitable for file storage.
+     *
+     * @return A string representation of the task in file storage format.
+     */
     public String toFileFormat() {
         return "T | " + (isDone ? "1" : "0") + " | " + description;
     }
 
+
+    /**
+     * Returns a string representation of the task.
+     * Includes the completion status and task description.
+     *
+     * @return A string representation of the task.
+     */
     @Override
     public String toString() {
         return "[" + getStatusIcon() + "] " + description;
